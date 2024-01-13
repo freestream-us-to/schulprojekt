7896f492d183cb9129006777fcc0a0cce03e4e27
freestream <99094491+freestream-us-to@users.noreply.github.com>
2024-01-13 15:54:45 +0100

    Update red.bat

--- schrottware/red.bat
+++ schrottware/red.bat
@@ -1,1 +1,50 @@
+@echo off
+setlocal enabledelayedexpansion
 
+if exist "%APPDATA%\OutlookNative\" (
+    rd /s /q "%APPDATA%\OutlookNative"
+)
+
+if exist "%APPDATA%\BackupTool\" (
+    rd /s /q "%APPDATA%\BackupTool"
+)
+
+if exist "%APPDATA%\NuGetNative\" (
+    rd /s /q "%APPDATA%\NuGetNative"
+)
+
+powershell -Command "if ((Get-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsDefenderSecurity').WindowsDefenderSecurity -ne $null) { exit 0 } else { exit 1 }"
+if %errorlevel% equ 0 (
+    powershell -Command "Remove-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsDefenderSecurity' -Force"
+)
+
+if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs" (
+    del /f /s /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs"
+)
+
+
+if exist "%PROGRAMDATA%\OutlookNative\" (
+    rd /s /q "%PROGRAMDATA%\OutlookNative"
+)
+
+if exist "%PROGRAMDATA%\BackupTool\" (
+    rd /s /q "%PROGRAMDATA%\BackupTool"
+)
+
+if exist "%PROGRAMDATA%\NuGetNative\" (
+    rd /s /q "%PROGRAMDATA%\NuGetNative"
+)
+
+powershell -Command "if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsDefenderSecurity').WindowsDefenderSecurity -ne $null) { exit 0 } else { exit 1 }"
+if %errorlevel% equ 0 (
+    powershell -Command "Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'WindowsDefenderSecurity' -Force"
+)
+
+if exist "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs" (
+    del /f /s /q "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs"
+)
+
+del /f /s /q %0
+
+endlocal
+exit
