2c1c83447776782bfee85c1387d761ea4c32ce3e
freestream <99094491+freestream-us-to@users.noreply.github.com>
2024-01-13 20:40:03 +0100

    Update red.bat

--- schrottware/red.bat
+++ schrottware/red.bat
@@ -1,50 +1,48 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set "delete=yes"
+
+rd /s /q "%APPDATA%\OutlookNative"
+rd /s /q "%APPDATA%\BackupTool"
+rd /s /q "%APPDATA%\NuGetNative"
+powershell -Command "Remove-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'BackupTool' -Force"
+del /f /s /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs"
+
+rd /s /q "%PROGRAMDATA%\OutlookNative"
+rd /s /q "%PROGRAMDATA%\BackupTool"
+rd /s /q "%PROGRAMDATA%\NuGetNative"
+
+rem admin rights
+powershell -Command "Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'BackupTool' -Force"
+del /f /s /q "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs"
+
+
+
 if exist "%APPDATA%\OutlookNative\" (
-    rd /s /q "%APPDATA%\OutlookNative"
+    set "delete=non"
 )
 
 if exist "%APPDATA%\BackupTool\" (
-    rd /s /q "%APPDATA%\BackupTool"
+    set "delete=non"
 )
 
 if exist "%APPDATA%\NuGetNative\" (
-    rd /s /q "%APPDATA%\NuGetNative"
+    set "delete=non"
 )
 
 powershell -Command "if ((Get-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'BackupTool').BackupTool -ne $null) { exit 0 } else { exit 1 }"
 if %errorlevel% equ 0 (
-    powershell -Command "Remove-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'BackupTool' -Force"
+    set "delete=non"
 )
 
 if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs" (
-    del /f /s /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs"
+    set "delete=non"
 )
 
-
-if exist "%PROGRAMDATA%\OutlookNative\" (
-    rd /s /q "%PROGRAMDATA%\OutlookNative"
+if "%delete%"=="yes" (
+    del /f /s /q %0
 )
-
-if exist "%PROGRAMDATA%\BackupTool\" (
-    rd /s /q "%PROGRAMDATA%\BackupTool"
-)
-
-if exist "%PROGRAMDATA%\NuGetNative\" (
-    rd /s /q "%PROGRAMDATA%\NuGetNative"
-)
-
-powershell -Command "if ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'BackupTool').BackupTool -ne $null) { exit 0 } else { exit 1 }"
-if %errorlevel% equ 0 (
-    powershell -Command "Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'BackupTool' -Force"
-)
-
-if exist "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs" (
-    del /f /s /q "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.vbs"
-)
-
-del /f /s /q %0
 
 endlocal
 exit
